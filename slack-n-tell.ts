4daa98a65f607d315cf0105d575525b194b12298
tsteele <tsteele@madisoncollege.edu>
2018-02-08 16:51:14 -0600

    Added slack-n-tell starter file.

---
+++ slack-n-tell.ts
@@ -0,0 +1,54 @@
+//
+// Create an array with only the postive values
+// given the following array of numbers to start
+// with.
+//
+const startingNumbers = [-3, -2, -1, 0, 1, 2, 3];
+
+// const positiveNumbers = startingNumber.
+
+// console.log(positiveNumbers);
+
+// Expect [1, 2, 3]
+
+
+
+
+
+
+
+
+//
+// Convert the following array of objects
+// to an array of objects that includes a
+// boolean 'checked' property so that it
+// can be bound to a list of checkboxes for
+// the user to choose their pizza toppings.
+//
+// Similar to our cat example in map-filter-reduce
+// branch.
+//
+const availableToppings = [
+    { name: "Sausage", cost: 0.50 }
+    , { name: "Cheese", cost: 0.0 }
+    , { name: "Mushrooms", cost: 1.00 }
+];
+
+// const availableToppingsWithCheckedProperty = availableToppings.
+
+//console.log(availableToppingsWithCheckedProperty)
+
+// [
+//     { name: "Sausage", cost: 0.50, checked: false }
+//     , { name: "Cheese", cost: 0.0, checked: false }
+//     , { name: "Mushrooms", cost: 1.00, checked: false }
+// ]
+
+
+//
+// Extra credit. Use .reduce to sum the startingNumbers array
+// from above.
+//
+
+//const extraCreditSum = startingNumbers.
+//console.log(extraCreditSum);
